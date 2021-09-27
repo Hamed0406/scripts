455da11488e7285999d0c36c1e66a97174bc8ebf
Hamed0406 <46892524+Hamed0406@users.noreply.github.com>
2021-09-27 11:51:58 +0200

    Update progeramming.sh

--- progeramming.sh
+++ progeramming.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 sudo apt update -y  #Update all repo with yes  to all answer
 sudo apt-get install git  # git version handeling
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install default-jre
-sudo apt-get install maven
+sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install default-jre -y
+sudo apt-get install maven -y
 sudo apt update -y 
-sudo apt install software-properties-common apt-transport-https wget 
+sudo apt install software-properties-common apt-transport-https wget -y
 sudo wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -  #import the Microsoft GPG key using the following wget command
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"  #  enable the Visual Studio Code repository
-sudo  apt update
-sudo apt install code # instal VSC 
+sudo  apt update -y
+sudo apt install code -y # instal VSC 
 sudo apt upgrade -y
 
 
